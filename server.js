5d20d3a8bce1273c1f27c4a1c50e1226acf381b1
Lucas Santiago Santana <lucin189@gmail.com>
2023-04-13 21:23:34 -0300

    Add helmet and configure HTTPS self signed certificates

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const fs = require("fs");
+const https = require("https");
 const path = require("path");
 const express = require("express");
+const helmet = require("helmet");
 
 const PORT = 8080;
 
 const app = express();
+
+app.use(helmet());
 
 app.get("/", (req, res) => {
   return res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -13,6 +18,14 @@ app.get("/secret", (req, res) => {
   return res.send("Your personal secret value is 42!");
 });
 
-app.listen(8080, () => {
-  console.log(`Server running at port: ${PORT}`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(8080, () => {
+    console.log(`Server running at port: ${PORT}`);
+  });
