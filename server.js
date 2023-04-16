7e49ff5a16131eb256fcb96a0238216d212ddec4
Lucas Santiago Santana <lucin189@gmail.com>
2023-04-15 23:31:22 -0300

    Create auth related endpoints boilerplate

--- server.js
+++ server.js
@@ -4,17 +4,31 @@ const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 
+const { checkLoggedIn } = require("./middlewares/checkLoggedIn.js")
+
 const PORT = 8080;
 
 const app = express();
 
 app.use(helmet());
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   return res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/secret", (req, res) => {
+app.get("/auth/google", (req, res) => {
+
+})
+
+app.get("/auth/google/callback", (req, res) => {
+
+})
+
+app.get('/auth/logout', (req, res) => {
+
+})
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
 });
 
