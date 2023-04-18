b220fd3afb8e6e85e5c0a883dc51f0aebd74e75d
Lucas Santiago Santana <lucin189@gmail.com>
2023-04-17 21:04:51 -0300

    Add cookie-session config, with user serialization and deserialization

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const express = require("express");
 const helmet = require("helmet");
 const passport = require("passport");
 const { Strategy } = require("passport-google-oauth20");
+const cookieSession = require("cookie-session");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const { checkLoggedIn } = require("./middlewares/checkLoggedIn.js");
 
@@ -14,24 +15,48 @@ const PORT = 8080;
 
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log('Google profile:', profile);
+  console.log("Google profile:", profile);
   done(null, profile);
-};
+}
 
-passport.use(new Strategy({
-  clientID: config.CLIENT_ID,
-  clientSecret: config.CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
-}, verifyCallback));
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+passport.use(
+  new Strategy(
+    {
+      clientID: config.CLIENT_ID,
+      clientSecret: config.CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    verifyCallback
+  )
+);
 
 const app = express();
 
-app.use(passport.initialize());
 app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (_, res) => {
   return res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -41,21 +66,26 @@ app.get("/failure", (_, res) => {
   return res.send("Failed to login.");
 });
 
-app.get("/auth/google", passport.authenticate("google", {
-  scope: ['email']
-}));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
 
-app.get("/auth/google/callback", passport.authenticate('google', {
-  failureRedirect: '/failure',
-  successRedirect: '/',
-  session: false
-}), (req, res) => {
-  console.log("Google called us back.")
-});
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: true,
+  }),
+  (req, res) => {
+    console.log("Google called us back.");
+  }
+);
 
-app.get('/auth/logout', (req, res) => {
-
-});
+app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
