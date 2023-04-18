13d7f7c8643a63347e01db29ac9bea1ca4f9722b
Lucas Santiago Santana <lucin189@gmail.com>
2023-04-17 21:12:37 -0300

    Implement checkLoggedIn middleware isAuthenticated valitation

--- middlewares/checkLoggedIn.js
+++ middlewares/checkLoggedIn.js
@@ -1,11 +1,11 @@
-function checkLoggedIn(_, res, next) {
-  const isLoggedIn = true;
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = req.isAuthenticated();
   if (!isLoggedIn) {
-    return res.status(401).json({ error: "You must login to continue." })
+    return res.status(401).json({ error: "You must login to continue." });
   }
   next();
 }
 
 module.exports = {
-  checkLoggedIn
-}
+  checkLoggedIn,
+};
